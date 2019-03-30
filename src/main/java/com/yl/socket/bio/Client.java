1fdc8407adc322c8f3a22e356ce0a545ac60c421
Alex <824187670@qq.com>
2019-03-30 19:40:27 +0800

    bio socket

--- src/main/java/com/yl/socket/bio/Client.java
+++ src/main/java/com/yl/socket/bio/Client.java
@@ -1,8 +1,6 @@
 package com.yl.socket.bio;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Inet4Address;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -16,7 +14,7 @@ public class Client {
     public static void main(String[] args) throws Exception{
         Socket client = new Socket();
         // 超时时间
-//        client.setSoTimeout(10000);
+        client.setSoTimeout(10000);
 
         // 连接服务端,连接超时时间3000
         client.connect(new InetSocketAddress(Inet4Address.getLocalHost(), 8083), 3000);
@@ -34,27 +32,29 @@ public class Client {
 
     private static void send(Socket client) throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-
+        PrintStream printStream = new PrintStream(client.getOutputStream());
         // 得到socket输出流
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
         boolean flag = true;
         do {
             // 获取键盘输入内容
             String sendInfo = input.readLine();
             // 发送数据
-            client.getOutputStream().write(sendInfo.getBytes());
-
+            printStream.println(sendInfo);
             // 获取服务端响应数据
             InputStream socketInput = client.getInputStream();
-            byte[] bytes = new byte[1024];
-            StringBuilder sb = new StringBuilder("服务端响应数据:");
-            int len = 0;
-            while ((len = socketInput.read(bytes)) > 0){
-                sb.append(new String(bytes,0,len));
-            }
-            if("bye".equalsIgnoreCase(sb.toString())){
+//            byte[] bytes = new byte[1024];
+//            StringBuilder sb = new StringBuilder("服务端响应数据:");
+//            int len = 0;
+//            while ((len = socketInput.read(bytes)) > 0){
+//                sb.append(new String(bytes,0,len));
+//            }
+            String res = bufferedReader.readLine();
+            if("bye".equalsIgnoreCase(res)){
                 flag = false;
             }else{
-                System.err.println(sb.toString());
+                System.err.println(res);
             }
         }while (flag);
 
