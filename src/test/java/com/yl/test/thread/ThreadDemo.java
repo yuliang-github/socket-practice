c723eec01421046023bd9053ec8721d01b9f2ea6
alex <824187670@qq.com>
2019-04-30 10:34:22 +0800

    LockSupport demo

--- src/test/java/com/yl/test/thread/ThreadDemo.java
+++ src/test/java/com/yl/test/thread/ThreadDemo.java
@@ -1,7 +1,12 @@
 package com.yl.test.thread;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.LockSupport;
 import java.util.concurrent.locks.ReentrantLock;
@@ -81,6 +86,56 @@ public class ThreadDemo {
         byte b1 = bytes[1];
 
         System.err.println(bytes[1]);
+
+    }
+
+    @Test
+    public void demo_2(){
+        List<Map<String,Object>> list = new ArrayList<>();
+        Map<String,Object> m1 = new HashMap<>();
+        m1.put("name", "miss");
+        m1.put("age", "18");
+        list.add(m1);
+
+        Map<String,Object> m2 = new HashMap<>();
+        m2.put("name", "zack");
+        m2.put("age", "30");
+        list.add(m2);
+
+        System.err.println(list.toString());
+
+        Integer i = 100;
+        System.err.println(i.toString());
+
+    }
+
+    @Test
+    public void demo_3(){
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.err.println(Lists.partition(list, 3));
+
+    }
+
+    @Test
+    public void demo_4(){
+
+        try {
+
+            try {
+                int i = 1/0;
+            }catch (Exception e){
+                System.err.println("里面catch");
+            }
+
+        }catch (Exception e){
+            System.err.println("外面catch");
+        }
 
     }
 }
